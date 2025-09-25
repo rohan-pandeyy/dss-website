7b677f5d65996452e753ffe21b474b6140df01c2
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2025-09-25 20:21:43 +0530

    Add interactive globe visualization components

--- src/components/ui/resizable-navbar.tsx
+++ src/components/ui/resizable-navbar.tsx
@@ -69,7 +69,7 @@ export const Navbar = ({ children, className }: NavbarProps) => {
     <motion.div
       ref={ref}
       // IMPORTANT: Change this to class of `fixed` if you want the navbar to be fixed
-      className={cn("sticky inset-x-0 top-5 z-40 w-full", className)}
+      className={cn("fixed inset-x-0 top-5 z-50 w-full", className)}
     >
       {React.Children.map(children, (child) =>
         React.isValidElement(child)
@@ -237,7 +237,7 @@ export const NavbarLogo = () => {
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
       <img
-        src="https://assets.aceternity.com/logo-dark.png"
+        src="/DSS_Logo.png"
         alt="logo"
         width={30}
         height={30}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import GlobeDemo from "@/components/globe-demo";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <GlobeDemo />
+      <div style={{ height: "200vh" }} />
     </div>
   );
 }
