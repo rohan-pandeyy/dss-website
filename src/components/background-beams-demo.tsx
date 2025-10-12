6451cf869892035b7b25955a0de00209676e2e01
ROHAN PANDEY <95585299+rohan-pandeyy@users.noreply.github.com>
2025-10-13 00:19:31 +0530

    Update background-beams-demo.tsx

--- src/components/background-beams-demo.tsx
+++ src/components/background-beams-demo.tsx
@@ -58,14 +58,14 @@ export default function BackgroundBeamsDemo() {
     <div className="h-[25rem] md:h-[40rem] w-full rounded-md bg-black relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-4xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          Junior Core Waitlist
+          Core Members&apos; Waitlist
         </h1>
         <p></p>
         <p className="text-neutral-500 max-w-lg mx-auto my-2 text-md text-center relative z-10">
           Sign up for our waitlist and we&apos;ll notify you on your college
-          email as soon as applications go live. Don&apos;t miss your chance to
-          be among the first to join our team, learn hands-on skills, and
-          contribute to exciting projects and events!
+          email as soon as application forms for core team go live. Don&apos;t
+          miss your chance to be among the first to join our team, learn
+          hands-on skills, and contribute to exciting projects and events!
         </p>
         <div className="flex flex-col items-center w-full">
           <div className="flex flex-row w-full items-center">
